2684829ec492db88c1ea3e95cfbfa9d6adc213b0
Osterloh <johnatanosterloh@gmail.com>
2022-05-16 20:51:21 -0300

    Create exemplo.c

---
+++ exemplo.c
@@ -0,0 +1,13 @@
+
+
+main(){
+    //Limite de crédito:
+    float limite[100];
+    //.....
+
+    printf("Informe o valor do limite: ")
+    scanf("%f", limite[i]);
+    if(limite[i] >= 100 && limite[i] <= 500)
+        //validação
+
+}
